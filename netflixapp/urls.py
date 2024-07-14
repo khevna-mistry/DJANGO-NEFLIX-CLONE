0b0b684b76e75651a2dc07cc0411f5a18944cd6f
@CODDER <139071502+khevna-mistry@users.noreply.github.com>
2024-07-14 15:34:51 +0530

    Add files via upload

---
+++ netflixapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import Home, ProfileList, ProfileCreate, MovieList, MovieDetail, PlayMovie
+
+app_name = 'netflixapp'
+
+urlpatterns = [
+    path('', Home.as_view(), name="Home"),
+    path('profiles/', ProfileList.as_view(), name="profile-list"),
+    path('profiles/create/', ProfileCreate.as_view(), name="profile-create"),
+    path('watch/<str:profile_id>/', MovieList.as_view(), name="movie-list"),
+    path('watch/detail/<str:movie_id>/', MovieDetail.as_view(), name="movie-detail"),
+    path('watch/play/<str:movie_id>/', PlayMovie.as_view(), name="play-movie"),
+      
+    #path('movies/',movies.as_view(),name='movies'),
+    #path('new_&_popular/',new_pop.as_view(),name='new_pop'),
+
+]
